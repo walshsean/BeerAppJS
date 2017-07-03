18d621d11ce3f5c6d82a04ed4830b9adc155ce57
walshsean <these.seans@gmail.com>
2017-07-03 14:44:24 -0400

    fixed spacing and quote consistency

--- application.js
+++ application.js
@@ -3,12 +3,12 @@ var Beer = {};
 Beer.Model = (function(){
 
 	var elements = {
-		container: document.querySelector(".beer-background"),
-		resultsContainer: document.querySelector(".beer-results"),
-		button: document.getElementById("beer-search-button"),
-	  form: document.querySelector(".beer-form"),
-	  input: document.querySelectorAll('input'),
-	  closeOutButton: document.querySelector(".beer-modal-close")
+		container: document.querySelector('.beer-background'),
+		resultsContainer: document.querySelector('.beer-results'),
+		button: document.getElementById('beer-search-button'),
+		form: document.querySelector('.beer-form'),
+		input: document.querySelectorAll('input'),
+		closeOutButton: document.querySelector('.beer-modal-close')
 	};
 
 	
